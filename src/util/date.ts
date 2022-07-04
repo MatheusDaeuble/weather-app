df7fd92dc7d3c38453c1e9a215071ccfcce99303
Matheus Daeuble <matheusdaeuble@gmail.com>
2022-07-04 19:42:08 -0300

    Add util functions

---
+++ src/util/date.ts
@@ -0,0 +1,24 @@
+export const months = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+];
+
+export const week = [
+  'Domingo',
+  'Segunda',
+  'Terça',
+  'Quarta',
+  'Quinta',
+  'Sexta',
+  'Sábado',
+];
---
+++ src/util/format.ts
@@ -0,0 +1,19 @@
+import { months, week } from './date';
+
+export const formatWeekDay = (date: Date | number) =>
+  week[new Date(date).getUTCDay()];
+
+// Example: 24°
+export const formatTemperature = (value: number) => `${Math.round(value)}°`;
+
+// Example: Seg, 4 de Julho de 2022
+export const formatDate = (value: Date | number) => {
+  const date = new Date(value);
+
+  const weekDay = formatWeekDay(date).slice(0, 3);
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${weekDay}, ${day} de ${month} de ${year}`;
+};
