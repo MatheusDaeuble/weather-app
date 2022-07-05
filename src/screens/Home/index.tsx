fa4c4592df26ba942badc97edcfc334285eacd96
Matheus Daeuble <matheusdaeuble@gmail.com>
2022-07-05 13:09:24 -0300

    Add template in home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,24 @@
+import React, { useContext } from 'react';
+import WeatherContext from 'contexts/Weather';
+import ShowWeatherTemplate from 'ui/components/templates/ShowWeatherTemplate';
+
 const HomeScreen = () => {
-  return null;
+  const { current, forecastWeek } = useContext(WeatherContext);
+
+  const doRefresh = () => {
+    current.refetch();
+    forecastWeek.refetch();
+  };
+
+  return (
+    <ShowWeatherTemplate
+      currentWeather={current.data}
+      forecastWeek={forecastWeek.data}
+      forecastLoading={forecastWeek.isFetching}
+      refreshing={current.isFetching || forecastWeek.isFetching}
+      onRefresh={doRefresh}
+    />
+  );
 };
 
 export default HomeScreen;
